c6f0143e2efb27f99423b6e4f5d421ce4ff1be89
deepa2307 <barmeda2307@gmail.com>
2023-11-30 11:03:59 +0530

    Lets Scale the app to more users

--- obj.js
+++ obj.js
@@ -1,10 +1,48 @@
-let myObj = {
-    name : "deepa";
-    age: "26"
+const form = document.getElementById("my-form");
+const nameIn = form["name"];
+const emailIn = form["email"];
+const numberIn = form["number"];
+const main = document.querySelector(".item");
+
+const submission = [];
+
+const addSubmission = (name, email, number) => {
+  submission.push({
+    name,
+    email,
+    number,
+  });
+  return { name, email, number };
 };
 
-let myObj_serialized = JSON.stringify(myObj);
+form.onsubmit = (e) => {
+  e.preventDefault();
 
-localStorage.setItem("myObj", myObj_serialized);
+  // Retrieve existing submissions from localStorage
+  let existingSubmissions = JSON.parse(localStorage.getItem("myObj")) || [];
 
-let myObj_real =JSON.parse(localStorage.getItem("myObj"))
+  // Add the new submission to the array
+  const newSub = addSubmission(nameIn.value, emailIn.value, numberIn.value);
+  existingSubmissions.push(newSub);
+
+  // Save the updated array back to localStorage
+  localStorage.setItem("myObj", JSON.stringify(existingSubmissions));
+
+  // Optionally, you can clear the form fields after submission
+  form.reset();
+};
+let displayData = () => {
+  let userData = JSON.parse(localStorage.getItem("myObj")) || [];
+
+  var final = "";
+
+  userData.forEach((element, i) => {
+    final += `<div class="items">${element.name} ${element.number} ${element.email}</div>`;
+  });
+
+  // Assuming there's a container with the id "displayContainer" where you want to append the final HTML
+  document.getElementsByClassName("item").innerText = final;
+};
+
+// Call the function to display data
+displayData();
